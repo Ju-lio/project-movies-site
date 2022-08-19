3d9149581269f022ef4d808c04dc68f7bc1fc488
Juyo <juyocastr@gmail.com>
2022-08-19 16:59:31 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project-movies-site';
+
+  filmes = [] as {
+    "titulo": string
+    "src": string
+    "ano": string
+    "duracao": string
+  }[]
+
+  title = 'project-movies-site'
+
+  importar = true
+
+  carregaFilmes() {
+
+    if(this.importar){
+
+      this.importar = false;
+
+      this.filmes.push({
+        titulo  : 'NIGHTCRAWLER',
+        src     : '../assets/mais-acess-00.png',
+        ano     : '2014',
+        duracao : '1h 57m' 
+      }) 
+
+      this.filmes.push({
+        titulo  : 'FIGHT CLUB',
+        src     : '../assets/mais-acess-01.png',
+        ano     : '1999',
+        duracao : '2h 19m'
+      })
+
+      this.filmes.push({
+        titulo  : 'NO COUNTRY FOR OLD MAN',
+        src     : '../assets/mais-acess-02.png',
+        ano     : '2007 ',
+        duracao : '2h 2m'
+      })
+
+      this.filmes.push({
+        titulo  : 'NOCTURNAL ANIMALS',
+        src     : '../assets/mais-acess-03.png',
+        ano     : '2016 ',
+        duracao : '1h 19m'
+      })
+
+      this.filmes.push({
+        titulo  : 'VIVARIUM',
+        src     : '../assets/mais-acess-04.png',
+        ano     : '2019  ',
+        duracao : '1h 37m'
+      })
+
+      this.filmes.push({
+        titulo  : 'MARROWBONE',
+        src     : '../assets/mais-acess-05.png',
+        ano     : '2017 ',
+        duracao : '1h 50m'
+      })
+
+      this.filmes.push({
+        titulo  : 'DONNIE DARKO',
+        src     : '../assets/mais-acess-06.png',
+        ano     : '2001  ',
+        duracao : '1h 53m'
+      })
+
+      this.filmes.push({
+        titulo  : 'CONJURING',
+        src     : '../assets/mais-acess-07.png',
+        ano     : '2013',
+        duracao : '1h 52m'
+      })
+    }
+  }
+
 }
--- src/app/filme/filme.component.ts
+++ src/app/filme/filme.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit,  } from '@angular/core';
 
 @Component({
   selector: 'app-filme',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmeComponent implements OnInit {
 
+  @Input() maisAcessTitulo : string =''           
+  @Input() maisAcessSrc    : string =''            
+  @Input() maisAcessAno    : string =''        
+  @Input() maisAcessDuracao: string =''   
+  
   constructor() { }
 
   ngOnInit(): void {
